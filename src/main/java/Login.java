3fef3f17f15b51d902c1709d959ff61632bce650
J4N8 <43821141+J4N8@users.noreply.github.com>
2021-03-15 12:57:55 +0100

    Added login and register tabs to Login.form Added GUI to login tab Added GUI to register tab

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+
 public class Login {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextField loginEmailTextField;
+    private JPasswordField loginPasswordTextField;
+    private JButton loginButton;
+    private JButton registerButton;
 }
