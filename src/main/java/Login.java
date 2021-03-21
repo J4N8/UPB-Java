5808136fdaff7f566d263722b765a8d07cb0b97f
MaksBah <76478924+MaksBah@users.noreply.github.com>
2021-03-21 18:41:01 +0100

    Added MD5 password hashing.

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,5 +1,11 @@
 import javax.swing.*;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
 public class Login {
     private JTabbedPane tabbedPane1;
     private JPanel panel1;
@@ -11,6 +17,7 @@ public class Login {
     private JPasswordField registerPasswordField;
     private JTextField registerNameTextField;
     private JTextField registerSurnameTextField;
+    private JComboBox registerCityComboBox;
 
     public Login() {
         JFrame jframe = new JFrame("UPB-Java");
@@ -19,5 +26,67 @@ public class Login {
         jframe.pack();
         jframe.setSize(1050, 400);
         jframe.setVisible(true);
+
+        setActionListeners();
+
+        //fill city combobox with items from database
+        loadCities();
     }
+    public static String doHashing (String password) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+
+            messageDigest.update(password.getBytes());
+
+            byte[] resultByteArray = messageDigest.digest();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (byte b : resultByteArray) {
+                sb.append(String.format("%02x", b));
+            }
+
+            return sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    private void setActionListeners(){
+        //register button on click
+        registerButton.addActionListener(e ->{
+            if (database.registerNewUser(registerEmailTextField.getText().trim(), doHashing(registerPasswordField.getText()), registerNameTextField.getText(), registerSurnameTextField.getText(), registerCityComboBox.getSelectedItem().toString()) == true){
+                Messages.registerUserSuccessful(panel1);
+            }
+            else{
+                Messages.registerUserFailed(panel1);
+            }
+        });
+
+        //login button on click
+        loginButton.addActionListener(e -> {
+            if (database.loginUser(loginEmailTextField.getText(), doHashing(loginPasswordTextField.getText())) != 0){
+                Messages.loginUserSuccessful(panel1);
+            }
+            else{
+                Messages.loginUserFailed(panel1);
+            }
+        });
+
+    }
+
+    //loads city combobox items from database
+    private void loadCities(){
+        DefaultComboBoxModel model = new DefaultComboBoxModel();
+        model.addAll(database.selectAllCities());
+        ArrayList<String> cities = database.selectAllCities();
+        registerCityComboBox.setModel(model);
+    }
+
+
+
+
 }
--- src/main/java/ProductsForm.java
+++ src/main/java/ProductsForm.java
@@ -1,2 +1,32 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class ProductsForm {
+    private JPanel panel1;
+    private JTabbedPane ProductsTabbedPane;
+    private JPanel ProductsPanel;
+    private JPanel CartPanel;
+    private JList Productlist;
+
+    public ProductsForm(){
+        JFrame jframe = new JFrame("Products");
+        jframe.setContentPane(panel1);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.pack();
+        jframe.setSize(1050, 400);
+        jframe.setVisible(true);
+
+        ArrayList<Product> products =  database.selectAllProducts();
+
+        DefaultListModel<Product> demoList = new DefaultListModel<>();
+
+        for (Product product: products
+        ) {
+            demoList.addElement(product);
+        }
+        //teamsList = new JList(demoList);
+        Productlist.setModel(demoList);
+
+        System.out.println("teams demoList: " + demoList);
+    }
 }
